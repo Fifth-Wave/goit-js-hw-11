323682e2c8e5af4775f941fabca03e65954f0c14
Dmytro Yeskov <88843350+Fifth-Wave@users.noreply.github.com>
2022-02-14 11:28:58 +0200

    add some plugIns and intial markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './sass/main.scss';
+import fetchContent from './partials/fetchContent';
+import { handlebars } from 'hbs';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const elements = {
+  searchButton: document.querySelector('.search-form button'),
+  inputSearch: document.querySelector('.search-form input'),
+  loadmoreButton: document.querySelector('.load-more'),
+};
+
+//          smooth scroll
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
